c02fd7d4d40f297bd58dfa230e1713b0c6e25752
Vinicius Melo <vpena.dev@gmail.com>
2025-06-23 18:34:18 -0300

    Aula 02 - Tipos Primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = (int) 10000000000L;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+        String nome = "Goku";
+
+        System.out.println("A idade é "+ idade + " anos.");
+        System.out.println("char "+ caractere);
+        System.out.println("Oi, meu nome é " + nome);
+    }
+}
