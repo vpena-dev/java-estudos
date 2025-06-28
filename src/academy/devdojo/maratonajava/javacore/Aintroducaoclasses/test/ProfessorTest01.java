4e1a37d3d92dd6e0a9e23dbc3a3d0b4519161cd1
Vinicius Melo <vpena.dev@gmail.com>
2025-06-28 14:29:17 -0300

    Aula 09 - POO Coesão

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/ProfessorTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+
+        professor.nome = "Fallen";
+        professor.idade = 38;
+        professor.sexo = 'M';
+
+        System.out.println("Nome: " + professor.nome + " Idade: " + professor.idade + " Sexo: " + professor.sexo);
+    }
+}
