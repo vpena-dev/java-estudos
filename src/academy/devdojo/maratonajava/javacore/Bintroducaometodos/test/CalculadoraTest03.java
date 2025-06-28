234fe190927af5e5d0bf1d56584884ce8cc46f6c
Vinicius Melo <vpena.dev@gmail.com>
2025-06-28 15:22:36 -0300

    Aula 09 - POO Métodos03 Retorno01

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double resultado = calculadora.divideDoisNumeros(20, 2);
+        System.out.println(resultado);
+
+    }
+}
