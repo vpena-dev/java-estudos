968c790bf4d15a035e19ac3895992f9b06eec03e
Vinicius Melo <vpena.dev@gmail.com>
2025-06-30 23:16:43 -0300

    Aula 09 - POO Modificador de acesso private, get e set pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTeste01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTeste01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+
+//        pessoa.imprime();
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+
+}
