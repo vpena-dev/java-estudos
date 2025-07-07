a78210d940b5141cf9b0e886204ee51bcfa92a39
Vinicius Melo <vpena.dev@gmail.com>
2025-07-06 22:06:48 -0300

    Aula 09 - POO Construtores02 Sobrecarga

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,21 +5,31 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime() {}
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public Anime() {
+        System.out.println("Dentro do construtor sem argumentos");
+    }
 
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public String getNome() {
