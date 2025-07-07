f9f8634f3fbaa54d3965e8017706ee4be74bb788
Vinicius Melo <vpena.dev@gmail.com>
2025-07-06 22:25:16 -0300

    Aula 09 - POO Blocos de inicialização

---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        System.out.println(episodios);
+        for (int episodio : this.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
